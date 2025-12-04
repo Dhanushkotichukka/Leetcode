9c5d60c5126bdf27b725bc21f8581657d7b619b4
Dhanushkoti Chukka <147791860+Dhanushkotichukka@users.noreply.github.com>
2025-12-04 15:29:11 +0530

    Time: 3 ms (95.98%), Space: 46.8 MB (45.24%) - LeetHub

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+    int ct = 1;
+    int tot = 0;
+    int m = 0,f=0;
+    int [][] arr = new int[rows*cols][2];
+    int i = rStart, j=cStart;
+    if(i>=0 && i<rows && j<cols  && j>=0){
+        arr[m][0] = i;
+        arr[m][1] = j;
+        m++;
+        tot++;
+    }
+    while(tot!=rows*cols){
+        if(ct%2!=0){
+            f=0;
+            while(f<ct){
+                j++;
+                if(i>=0 && i<rows && j<cols && j>=0){
+                    arr[m][0] = i;
+                    arr[m][1] = j;
+                    m++;
+                    tot++;
+                }
+                f++;
+            }
+            f=0;
+            while(f<ct){
+                i++;
+                if(i>=0 && i<rows && j<cols && j>=0){
+                    arr[m][0] = i;
+                    arr[m][1] = j;
+                    m++;
+                    tot++;
+                }
+                f++;
+            }
+        }else{
+            f=0;
+            while(f<ct){
+                j--;
+                if(i>=0 && i<rows && j<cols && j>=0){
+                    arr[m][0] = i;
+                    arr[m][1] = j;
+                    m++;
+                    tot++;
+                }
+                f++;
+            }
+            f=0;
+            while(f<ct){
+                i--;
+                if(i>=0 && i<rows && j<cols && j>=0){
+                    arr[m][0] = i;
+                    arr[m][1] = j;
+                    m++;
+                    tot++;
+                }
+                f++;
+            }
+        }
+        ct++;
+    }
+    return arr;
+    }
+}
