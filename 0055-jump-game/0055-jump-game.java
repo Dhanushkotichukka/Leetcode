78c87436b2f06446becec5ce77585f1353d75d7a
Dhanushkoti Chukka <147791860+Dhanushkotichukka@users.noreply.github.com>
2026-02-18 19:44:26 +0530

    Time: 1 ms (99.82%), Space: 48.1 MB (22.35%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+         int ind = nums.length - 1;
+         for(int i=ind-1; i>=0 ;i--){
+            if(i+nums[i] >=  ind) ind = i;
+         }
+         return ind == 0;
+    }
+}
